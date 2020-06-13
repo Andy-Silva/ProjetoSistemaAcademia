7b21b7a31366da736e4af8026b753e1a76289eea
Andy-Silva <andy.ferreira@hotmail.com>
2020-06-13 20:17:46 -0300

    Projeto iniciado, criacao dos departamentos

---
+++ SistemaAcademia/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaAcademia.Models;
+
+namespace SistemaAcademia.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Professores" });
+            list.Add(new Department { Id = 2, Name = "Alunos" });
+
+            return View(list);
+        }
+    }
+}
