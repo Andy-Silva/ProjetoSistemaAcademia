c7d70506b4efd431a28d88c47bfbf7657a3ba69a
Andy-Silva <andy.ferreira@hotmail.com>
2020-06-14 10:22:05 -0300

    criacao db departments

---
+++ SistemaAcademia/Data/SistemaAcademiaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaAcademia.Models;
+
+namespace SistemaAcademia.Data
+{
+    public class SistemaAcademiaContext : DbContext
+    {
+        public SistemaAcademiaContext (DbContextOptions<SistemaAcademiaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaAcademia.Models.Department> Department { get; set; }
+    }
+}
